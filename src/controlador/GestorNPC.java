7cf0e9acc6f631d704ee0f8c559d981a221a2423
Jesús <ngrt0x@gmail.com>
2026-02-25 20:25:30 +0100

    Creados los NPC de la segunda isla

--- src/controlador/GestorNPC.java
+++ src/controlador/GestorNPC.java
@@ -31,7 +31,7 @@ public class GestorNPC {
 
 	String pistaAntonio = "Esto es una pista";
 
-	NPC antonio = new NPC("Antonio", dialogosAntonio,opcionesDialogoAntonio, primeraAntonio, true, pistaAntonio);
+	NPC antonio = new NPC("Antonio", dialogosAntonio, opcionesDialogoAntonio, primeraAntonio, true, pistaAntonio);
 
 	// Manuela
 	String[] dialogosManuela = { "Hoy hace un dia estupendo para dar un paseo",
@@ -49,23 +49,61 @@ public class GestorNPC {
 	NPC[] habitantesIsla1 = { richard, antonio, manuela, josefina }; // HABITANTES DE LA ISLA 1
 
 	// ============================
+	// ========== ISLA 2 ==========
+
+	// Recesvinto
+	String[] dialogosRecesvinto = { "La gente se burla de mi por tener un nombre muy raro",
+			"Me gusta cuando me acarician detras de las orejas", "Se me da muy bien la carpinteria" };
+	String primeraRecesvinto = "No les hagas caso a los demas, no soy tan rara como aparento";
+	NPC recesvinto = new NPC("Recesvinto", dialogosRecesvinto, primeraRecesvinto);
+
+	// Eduardo
+	String[] dialogosEduardo = { "Me gusta mucho el futbol", "Mi abuela tiene un gato muy bonito",
+			"Ven conmigo a jugar algun dia" };
+	String primeraEduardo = "Hola soy Eduardo tengo 9 años";
+	NPC eduardo = new NPC("Eduardo", dialogosEduardo, primeraEduardo);
+
+	// Moises // TIENE PISTA
+	String opcionesDialogoMoises = "1. Saludar\n2. Preguntar sobre el tiempo\n3. Hablar sobre Eduardo\n4. Oye he oido que tu separaste los mares ";
+	String[] dialogoMoises = { "Hey te veo bien", "Aqui siempre suele haber buen tiempo ni mucho frio ni mucho calor sabes", "Eduardo es un buen niño, seguro que se hace famoso y da a conocer nuestra isla"};
+
+	String primeraMoises = "Me llaman Moises, es un placer verte visitando esta humilde isla";
+
+	String pistaMoises = "Habla conmigo en otro momendo (En desarrollo, opcion no disponible)";
+
+	NPC moises = new NPC("Moises", dialogoMoises, opcionesDialogoMoises, primeraMoises, true, pistaMoises);
+
+	// Patricia
+	String[] dialogosPatricia = { "Tengo un nieto muy guapo llamado Eduardo",
+			"Seguro que a mi gata le encantarias", "Algun dia te traere galletas para que las pruebes" };
+	String primeraPatricia = "Hola corazon, es tu primera vez por aqui?";
+	NPC patricia = new NPC("Patricia", dialogosPatricia, primeraPatricia);
+	
+	NPC[] habitantesIsla2 = {recesvinto,eduardo,moises,patricia};
+	
+	// ============================
 
 	// getters
 
 	public NPC[] getHabitantesIsla1() {
 		return habitantesIsla1;
 	}
+	
+	public NPC[] getHabitantesIsla2() {
+		return habitantesIsla2;
+	}
 
 	// metodos propios
 
 	public void hablarNPC(Jugador j) {
 		NPC objetivo = vistaJuego.menuHabitantes(j.getIslaActual());
-		
+
 		boolean salir = false;
-		while(!salir) {
+		while (!salir) {
 			try {
 				if (!objetivo.isConocido()) {
-					gestorVista.imprimirMensaje(objetivo.getPrimeraFrase()); // si es la primera vez que hablas con el NPC
+					gestorVista.imprimirMensaje(objetivo.getPrimeraFrase()); // si es la primera vez que hablas con el
+																				// NPC
 																				// te
 																				// dira su primera frase
 					objetivo.setConocido(true);
@@ -85,14 +123,15 @@ public class GestorNPC {
 																				// diario
 							j.getDiario().agregarPista(objetivo.getPista());
 						} else {
-							gestorVista.imprimirMensaje(objetivo.getDialogos()[opcionDialogo - 1]); // si es una opcion normal dara
-																								// el
-																								// dialogo correspondiente
+							gestorVista.imprimirMensaje(objetivo.getDialogos()[opcionDialogo - 1]); // si es una opcion
+																									// normal dara
+							// el
+							// dialogo correspondiente
 						}
 					} else {// NPC normales de las islas con dialogos random
 						int random = RANDOM.nextInt(objetivo.getDialogos().length); // si no es la primera vez te dira
-																						// una
-																						// de sus diversas frases
+																					// una
+																					// de sus diversas frases
 						gestorVista.imprimirMensaje(objetivo.getDialogos()[random]);
 					}
 
